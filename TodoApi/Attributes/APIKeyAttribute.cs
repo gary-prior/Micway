8ddc1462d1febf971b9c266be45f82bd25f3c537
gary-prior <91433207+gary-prior@users.noreply.github.com>
2021-09-27 06:32:07 +1000

    Add files via upload

---
+++ TodoApi/Attributes/APIKeyAttribute.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Primitives;
+using System;
+using System.Threading.Tasks;
+
+namespace TodoApi.Attributes
+{
+    public class APIKeyAttribute : Attribute, IAsyncActionFilter
+    {
+        public async Task OnActionExecutionAsync(ActionExecutingContext context, ActionExecutionDelegate next)
+        {
+            if (!context.HttpContext.Request.Headers.TryGetValue("APIKey", out StringValues strAPIKey))
+            {
+                context.Result = new ContentResult()
+                {
+                    StatusCode = 401,
+                    Content = "No API key."
+                };
+                return;
+            }
+
+            IConfiguration appSettings = context.HttpContext.RequestServices.GetRequiredService<IConfiguration>();
+            string apiKey = appSettings.GetValue<string>("APIKey");
+
+            if (!apiKey.Equals(strAPIKey))
+            {
+                context.Result = new ContentResult()
+                {
+                    StatusCode = 401,
+                    Content = "Invalid API key."
+                };
+                return;
+            }
+
+            await next();
+        }
+    }
+}
---
+++ TodoApi/Models/TodoContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TodoApi.Models;
+
+namespace TodoApi.Models
+{
+    public class TodoContext : DbContext
+    {
+        public TodoContext(DbContextOptions<TodoContext> options) :base(options)
+        {
+
+        }
+        public DbSet<TodoApi.Models.TodoItem> TodoItem { get; set; }
+
+
+    }
+}
